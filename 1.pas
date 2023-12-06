cef314d60a4393e4e4f0e3fcf5bbfa545aa04cae
NssWay <115477023+NssWay@users.noreply.github.com>
2023-12-06 08:58:23 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,21 @@
+﻿program z1;
+var
+s:string;
+i,a,b:integer;
+begin
+  writeln('Введите число, большое ;))) ');
+  readln(s);
+  i:=length(s);
+  writeln('Длина строки ', i);
+ a:=2;
+  for a:=2 to i-2 do
+  begin
+    if (s[a]<s[a+2]) then
+    else
+    break;
+  end;
+  if a=i-2 then 
+    writeln('Монотонна')
+  else
+    writeln('Не монотонна');
+end.
